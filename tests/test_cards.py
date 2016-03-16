226685ff4495af134ddb413fdb989e0cf4492ada
Nim'Vek <nimvek@web.de>
2016-03-16 23:05:12 +0100

    test equals of card

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -5,5 +5,9 @@ from machikoro.card import Card
 
 
 def test_equal():
-    assert_equals(CardType.PRIMARY_INDUSTRY, Card.WHEAT_FIELD)
+    assert_equals(Card.WHEAT_FIELD, CardExpansion.BASE)
+    assert_equals(Card.WHEAT_FIELD, CardType.PRIMARY_INDUSTRY)
+    assert_equals(Card.WHEAT_FIELD, CardType.BLUE)
+    assert_equals(Card.WHEAT_FIELD, CardSymbol.GRAIN)
+    assert_equals(Card.WHEAT_FIELD, 1)
     assert_not_equals(CardType.SECONDARY_INDUSTRY, Card.WHEAT_FIELD)
