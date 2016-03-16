a864fed3c2a59983b5495be51017258d45e5632a
Nim'Vek <nimvek@web.de>
2016-03-16 21:54:26 +0100

    More constants

--- machikoro/constant.py
+++ machikoro/constant.py
@@ -6,6 +6,13 @@ This module contains the constants
 
 from enum import Enum
 
+class CardExpansion(Enum):
+    """Defining the Expansion"""
+    BASE = 1
+    HARBOUR = 2
+    SHARP = 3
+    PROMOTION = 4
+
 class CardType(Enum):
     """Defining the Establishment type"""
     PRIMARY_INDUSTRY = BLUE = 1
@@ -13,3 +20,16 @@ class CardType(Enum):
     RESTAURANT = RED = 3
     MAJOR_ESTABLISHMENT = PURPLE = 4
     LANDMARK = 5
+
+class CardSymbol(Enum):
+    """Defining the Establishment symbol"""
+    GRAIN = 1
+    ANIMAL = 2
+    GEAR = 3
+    SHOP = 4
+    FACTORY = 5
+    FRUIT = 6
+    CUP = 7
+    BOAT = 8
+    BRIEFCASE = 9
+    LANDMARK = 10
--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,5 +1,5 @@
 
-from nose.tools import assert_equals
+from nose.tools import *
 
 from machikoro.constant import *
 
@@ -8,3 +8,7 @@ def test_colors():
     assert_equals(CardType.SECONDARY_INDUSTRY, CardType.GREEN)
     assert_equals(CardType.RESTAURANT, CardType.RED)
     assert_equals(CardType.MAJOR_ESTABLISHMENT, CardType.PURPLE)
+
+def test_enum_differs():
+    assert_not_equals(CardType.BLUE, CardExpansion.BASE)
+    assert_not_equals(CardType.BLUE, CardSymbol.GRAIN)
