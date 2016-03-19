63aff26e6aa464eb50a2a9c2417bf70ded03d205
Nim'Vek <nimvek@web.de>
2016-03-19 14:10:17 +0100

    add container tests

--- tests/test_container.py
+++ tests/test_container.py
@@ -51,3 +51,38 @@ def test_limited_supply_count():
     assert_equals(supply.count(CardType.RED), 3)
     assert_equals(supply.count(CardType.PURPLE), 2)
     assert_equals(supply.count(CardType.LANDMARK), 2)
+
+def test_limited_supply_remove():
+    supply = LimitedSupply([Card.WHEAT_FIELD, Card.CAFE, Card.MINE,
+                              Card.STADIUM, Card.STATION], 2, 3)
+    assert_equals(supply.count(CardType.RED), 3)
+    assert_equals(supply.remove(CardType.RED), Card.CAFE)
+    assert_equals(supply.count(CardType.RED), 2)
+    assert_equals(supply.remove(CardType.RED), Card.CAFE)
+    assert_equals(supply.count(CardType.RED), 1)
+    assert_equals(supply.remove(CardType.RED), Card.CAFE)
+    assert_equals(supply.count(CardType.RED), 0)
+    assert_raises(KeyError, supply.remove, CardType.RED)
+
+    assert_equals(supply.count(CardType.BLUE), 6)
+    assert_in(supply.remove(CardType.BLUE), [Card.WHEAT_FIELD, Card.MINE])
+    assert_equals(supply.count(CardType.BLUE), 5)
+    assert_equals(supply.count(Card.STADIUM), 2)
+    assert_equals(supply.remove(Card.STADIUM), Card.STADIUM)
+    assert_equals(supply.count(Card.STADIUM), 1)
+
+    assert_in(supply.remove(), [Card.WHEAT_FIELD, Card.MINE, Card.STADIUM,
+                                Card.STATION])
+    assert_equals(
+        supply.remove(['&', CardType.BLUE, CardSymbol.GEAR]), Card.MINE)
+    assert_raises(KeyError, supply.remove, ['&', CardType.RED, CardSymbol.GEAR])
+
+def test_limited_supply_add():
+    supply = LimitedSupply([Card.WHEAT_FIELD, Card.CAFE, Card.STATION], 2)
+    assert_equals(supply.count(), 14)
+    supply.add(Card.CAFE)
+    assert_equals(supply.count(), 15)
+    assert_equals(supply.count(CardType.BLUE), 6)
+    supply.add(Card.MINE)
+    assert_equals(supply.count(CardType.BLUE), 7)
+    assert_raises(AssertionError, supply.add, CardType.PURPLE)
